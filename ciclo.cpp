f60e0fa421dd2948986cfa45dabd3facc03454bf
lai2-01 <lai2-01@itismeucci.com>
2022-02-15 11:31:57 +0100

    modifiche ciclo

---
+++ ciclo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std();
+main(){
+int contatore = 0;
+for(int i = 0; i < 10; i++){
+contatore++;
+}
+
+cout<<"Questo e' iltuo contatore :" << contatore << endl;
+
+
+
+}
